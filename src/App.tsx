e583d8f61cc81ec2f0012f803c43e50de9ff16d7
jaeyo03 <toyazx@naver.com>
2025-02-22 02:04:06 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { Button, Fieldset, Input, Stack } from "@chakra-ui/react"
+import { Field } from "@/components/ui/field"
+import { createListCollection } from "@chakra-ui/react"
+import {
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+} from "@/components/ui/select"
+
+const frameworks = createListCollection({
+  items: [
+    { label: "React.js", value: "react" },
+    { label: "Vue.js", value: "vue" },
+    { label: "Angular", value: "angular" },
+    { label: "Svelte", value: "svelte" },
+  ],
+})
+
+export default function App(){
+  return (
+    <Fieldset.Root size="lg" maxW="md">
+      <Stack>
+        <Fieldset.Legend>Contact details</Fieldset.Legend>
+        <Fieldset.HelperText>
+          Please provide your contact details below.
+        </Fieldset.HelperText>
+      </Stack>
+
+      <Fieldset.Content>
+        <Field label="Name">
+          <Input name="name" />
+        </Field>
+
+        <Field label="Email address">
+          <Input name="email" type="email" />
+        </Field>
+
+        <Field label="Country">
+          <SelectRoot collection={frameworks} size="sm" width="320px">
+            <SelectLabel>Select framework</SelectLabel>
+            <SelectTrigger>
+              <SelectValueText placeholder="Select movie" />
+            </SelectTrigger>
+            <SelectContent>
+              {frameworks.items.map((movie) => (
+                <SelectItem item={movie} key={movie.value}>
+                  {movie.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </SelectRoot>
+        </Field>
+      </Fieldset.Content>
+
+      <Button type="submit" alignSelf="flex-start">
+        Submit
+      </Button>
+    </Fieldset.Root>
+  )
+}
