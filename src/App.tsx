0992f87fe2f4e418c65e9debbfc09d6e57d72f25
jaeyo03 <toyazx@naver.com>
2025-02-23 23:45:39 +0900

    feat : 레이아웃 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,22 @@
-import { Button, Fieldset, Input, Stack } from "@chakra-ui/react"
-import { Field } from "@/components/ui/field"
-import { createListCollection } from "@chakra-ui/react"
-import {
-  SelectContent,
-  SelectItem,
-  SelectLabel,
-  SelectRoot,
-  SelectTrigger,
-  SelectValueText,
-} from "@/components/ui/select"
-
-const frameworks = createListCollection({
-  items: [
-    { label: "React.js", value: "react" },
-    { label: "Vue.js", value: "vue" },
-    { label: "Angular", value: "angular" },
-    { label: "Svelte", value: "svelte" },
-  ],
-})
+import MainPage from "@/pages/MainPage";
+import SideBar from "@/components/common/SideBar.tsx";
+import styled from "@emotion/styled";
+import Header from "@/components/common/Header.tsx";
 
 export default function App(){
   return (
-    <Fieldset.Root size="lg" maxW="md">
-      <Stack>
-        <Fieldset.Legend>Contact details</Fieldset.Legend>
-        <Fieldset.HelperText>
-          Please provide your contact details below.
-        </Fieldset.HelperText>
-      </Stack>
-
-      <Fieldset.Content>
-        <Field label="Name">
-          <Input name="name" />
-        </Field>
-
-        <Field label="Email address">
-          <Input name="email" type="email" />
-        </Field>
-
-        <Field label="Country">
-          <SelectRoot collection={frameworks} size="sm" width="320px">
-            <SelectLabel>Select framework</SelectLabel>
-            <SelectTrigger>
-              <SelectValueText placeholder="Select movie" />
-            </SelectTrigger>
-            <SelectContent>
-              {frameworks.items.map((movie) => (
-                <SelectItem item={movie} key={movie.value}>
-                  {movie.label}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </SelectRoot>
-        </Field>
-      </Fieldset.Content>
-
-      <Button type="submit" alignSelf="flex-start">
-        Submit
-      </Button>
-    </Fieldset.Root>
+    <>
+      <Header/>
+      <Layout>
+        <SideBar />
+        <MainPage/>
+      </Layout>
+    </>
   )
 }
+
+const Layout = styled.div`
+  display: flex;
+  height: 100vh;
+  background: #F4F4F5;
+`;
