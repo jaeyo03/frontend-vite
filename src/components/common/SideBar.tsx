375618e203baf858144458895b98c0d971ad7bde
jaeyo03 <toyazx@naver.com>
2025-02-24 03:34:17 +0900

    feat : router 기능 추가

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled'
-import SideBarMenu from "@/components/common/SideBarMenu.tsx";
+import {Link} from "react-router";
 
 export default function SideBar () {
   return (
@@ -10,7 +10,9 @@ export default function SideBar () {
           width="24"
           height="24"
         />
-        <SideBarMenu>자기소개서</SideBarMenu>
+        <SideBarMenu>
+          <Link to="/resume">자기소개서</Link>
+        </SideBarMenu>
       </SideBarWrap>
       <SideBarWrap>
         <img
@@ -18,7 +20,9 @@ export default function SideBar () {
           width="24"
           height="24"
         />
-        <SideBarMenu>휴지통</SideBarMenu>
+        <SideBarMenu>
+          <Link to="/trash">휴지통</Link>
+        </SideBarMenu>
       </SideBarWrap>
       <SideBarWrap>
         <img
@@ -26,22 +30,36 @@ export default function SideBar () {
           width="24"
           height="24"
         />
-        <SideBarMenu>계정 관리</SideBarMenu>
+        <SideBarMenu>
+          <Link to="/account">계정 관리</Link>
+        </SideBarMenu>
       </SideBarWrap>
     </SideLayout>
   )
 }
 
 const SideLayout = styled.div`
-  width: 320px;
+  width: 250px;
   height: 100%;
   background: white;
   border-style: solid;
   border-right-width: 1px;
+  padding-top :10px;
 `
 
 const SideBarWrap = styled.div`
   display: flex;
   width : fit-content;
   margin-left : 25px;
+`
+
+const SideBarMenu = styled.div`
+  font-size : 20px;
+  width: fit-content;
+  font-weight: bold;
+  color : #878787;
+  margin-left : 10px;
+  margin-top : 15px;
+  margin-bottom: 15px;
+  cursor: pointer;
 `
