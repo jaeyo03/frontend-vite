e8e5b7000905ba85e647a95a8a132e19e42b32cd
jaeyo03 <toyazx@naver.com>
2025-02-25 09:01:06 +0900

    style : 헤더 스타일 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,28 +3,16 @@ import styled from '@emotion/styled'
 export default function Header () {
   return (
     <HeaderLayout>
-      <img
-        src="/logo.svg"
-        alt="logo"
-        width="65"
-        height="65"
-      />
-      <Title>Jobextension</Title>
     </HeaderLayout>
   )
 }
 
 const HeaderLayout = styled.div`
-  height: 90px;
-  padding-left : 20px;
+  height: 100px;
+  width: calc(100vw - 306px);
   border-style: solid;
   border-bottom-width: 1px;
   align-items: center;
   display: flex;
-`
-
-const Title = styled.div`
-  font-size: 40px;
-  font-weight: bold;
-  color: #7676FF;
+  background: #FFFFFF;
 `
