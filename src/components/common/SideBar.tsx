c51bf6d6eb9144f4129be844b7c2a62faab79b12
jaeyo03 <toyazx@naver.com>
2025-02-25 09:02:23 +0900

    style : 사이드 바 레이아웃 스타일 수정

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -4,62 +4,96 @@ import {Link} from "react-router";
 export default function SideBar () {
   return (
     <SideLayout>
-      <SideBarWrap>
+      <TitleWrap>
         <img
-          src="/vite.svg"
-          width="24"
-          height="24"
+          src="/j-icon.svg"
+          alt="logo"
+          width="45"
+          height="45"
         />
-        <SideBarMenu>
-          <Link to="/resume">자기소개서</Link>
-        </SideBarMenu>
+        <Title>Jobextension</Title>
+      </TitleWrap>
+      <SideBarWrap>
+        <Link to="/resume" style={{ display: 'flex' }}>
+          <img
+            src="/resume-icon.svg"
+            width="24"
+            height="24"
+            alt="resume-icon"
+          />
+          <SideBarMenu>
+            자기소개서
+          </SideBarMenu>
+        </Link>
       </SideBarWrap>
       <SideBarWrap>
-        <img
-          src="/vite.svg"
-          width="24"
-          height="24"
-        />
-        <SideBarMenu>
-          <Link to="/trash">휴지통</Link>
-        </SideBarMenu>
+        <Link to="/trash" style={{ display: 'flex' }}>
+          <img
+            src="/trash-icon.svg"
+            width="24"
+            height="24"
+            alt="trash-icon"
+          />
+          <SideBarMenu>
+            휴지통
+          </SideBarMenu>
+        </Link>
       </SideBarWrap>
       <SideBarWrap>
-        <img
-          src="/vite.svg"
-          width="24"
-          height="24"
-        />
-        <SideBarMenu>
-          <Link to="/account">계정 관리</Link>
-        </SideBarMenu>
+        <Link to="/account" style={{ display: 'flex' }}>
+          <img
+            src="/account-icon.svg"
+            width="24"
+            height="24"
+            alt="account-icon"
+          />
+          <SideBarMenu>
+            계정 관리
+          </SideBarMenu>
+        </Link>
       </SideBarWrap>
     </SideLayout>
   )
 }
 
 const SideLayout = styled.div`
-  width: 250px;
+  width: 306px;
   height: 100%;
   background: white;
   border-style: solid;
   border-right-width: 1px;
-  padding-top :10px;
+  padding-top : 31px;
+  flex-shrink: 0;
+`
+
+const Title = styled.div`
+  font-size: 26px;
+  font-weight: bold;
+  color: #7676FF;
+  margin-left: 10px;
+`
+
+const TitleWrap = styled.div`
+  display: flex;
+  width : fit-content;
+  justify-content: center;
+  align-items: center;
+  margin-left : 25px;
 `
 
 const SideBarWrap = styled.div`
   display: flex;
   width : fit-content;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  margin-top : 40px;
   margin-left : 25px;
 `
 
 const SideBarMenu = styled.div`
   font-size : 20px;
-  width: fit-content;
   font-weight: bold;
   color : #878787;
   margin-left : 10px;
-  margin-top : 15px;
-  margin-bottom: 15px;
-  cursor: pointer;
 `
