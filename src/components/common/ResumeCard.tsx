b96308ca9592d3e4c8769e6a8b36ab715a483877
jaeyo03 <toyazx@naver.com>
2025-02-26 02:53:38 +0900

    refactor : resumeName을 title로 변수명 변경

--- src/components/common/ResumeCard.tsx
+++ src/components/common/ResumeCard.tsx
@@ -1,24 +1,23 @@
 import { Button, Card } from "@chakra-ui/react"
 import { Stat } from "@chakra-ui/react"
+import {Resume} from "@/types/type.ts";
 
-export interface ResumeCardProps {
-  resumeID : string;
-  resumeName : string;
-  questionsCount : number;
+interface ResumeCardProps extends Resume {
+  handleDetailClick: (id: string) => void;
 }
 
-export default function ResumeCard({ resumeName, questionsCount }: ResumeCardProps) {
+export default function ResumeCard({ resumeID, title, questionsCount, handleDetailClick }: ResumeCardProps) {
   return (
     <Card.Root width="320px" variant={'outline'} height="200px">
       <Card.Body gap="2">
-        <Card.Title mb="2">{resumeName}</Card.Title>
+        <Card.Title mb="2">{title}</Card.Title>
         <Stat.Root size="sm">
           <Stat.Label>자소서 수</Stat.Label>
           <Stat.ValueText>{questionsCount}</Stat.ValueText>
         </Stat.Root>
       </Card.Body>
       <Card.Footer justifyContent="flex-end">
-        <Button colorPalette="gray" variant="subtle">조회</Button>
+        <Button colorPalette="gray" variant="subtle" onClick={() => handleDetailClick(resumeID)}>조회</Button>
         <Button colorPalette="red" variant="subtle">삭제</Button>
       </Card.Footer>
     </Card.Root>
