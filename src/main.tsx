6a5c3940700ca043e0089c9bd701e237f02fab7a
jaeyo03 <toyazx@naver.com>
2025-02-26 02:52:54 +0900

    feat : 질의응답 페이지 라우팅 추가

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import {BrowserRouter, Route, Routes} from "react-router";
 import ResumePage from "@/pages/ResumePage";
 import TrashPage from "@/pages/TrashPage";
 import AccountPage from "@/pages/AccountPage";
+import QuestionDetailPage from "@/pages/QuestionDetailPage";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/" element = {<Layout/>}>
             <Route index element={<ResumePage />} />
             <Route path="resume" element={<ResumePage/>}></Route>
+            <Route path="/resume/:id" element={<QuestionDetailPage/>}></Route>
             <Route path="trash" element={<TrashPage/>}></Route>
             <Route path="account" element={<AccountPage/>}></Route>
           </Route>
