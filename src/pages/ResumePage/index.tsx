d7de6242e5c7b0bf894fcd599b4ff643e4c309a8
jaeyo03 <toyazx@naver.com>
2025-02-26 02:52:41 +0900

    feat : fetch 구현 및 임시 데이터 삭제

--- src/pages/ResumePage/index.tsx
+++ src/pages/ResumePage/index.tsx
@@ -1,25 +1,36 @@
-import ResumeCard, {ResumeCardProps} from "@/components/common/ResumeCard.tsx";
+import ResumeCard from "@/components/common/ResumeCard.tsx";
 import styled from "@emotion/styled";
-
-const tempData : ResumeCardProps[] = [
-  { resumeID : '123123', resumeName : '2025 LG전자', questionsCount : 1 },
-  { resumeID : '13432', resumeName : '잡탕', questionsCount : 4 },
-  { resumeID : '13423', resumeName : '2025 에릭슨엘지', questionsCount : 10 },
-  { resumeID : '412323', resumeName : '2025 삼성SDS', questionsCount : 7 },
-  { resumeID : '412323', resumeName : '2025 삼성SDS', questionsCount : 7 },
-  { resumeID : '412323', resumeName : '2025 삼성SDS', questionsCount : 7 },
-  { resumeID : '412323', resumeName : '2025 삼성SDS', questionsCount : 7 },
-];
+import {useNavigate} from "react-router";
+import {Resume} from "@/types/type.ts";
+import {useEffect, useState} from "react";
+import {getResumeData} from "@/apis/resumeAPI.ts";
 
 export default function ResumePage() {
+  const [resumeData, setResumeData] = useState<Resume[]>([]);
+  const navigate = useNavigate();
+
+  const handleDetailClick = (id: string) => {
+    navigate(`/resume/${id}`);
+  }
+
+  const initialResumeGet = async () => {
+    const data = await getResumeData();
+    setResumeData(data);
+  }
+
+  useEffect(() => {
+    initialResumeGet();
+  }, []);
+
   return (
     <ResumeLayout>
-      {tempData.map((item) => (
+      {resumeData.map((item) => (
         <ResumeCard
           key={item.resumeID}
           resumeID={item.resumeID}
-          resumeName={item.resumeName}
+          title={item.title}
           questionsCount={item.questionsCount}
+          handleDetailClick={handleDetailClick}
         />
       ))}
     </ResumeLayout>
@@ -34,4 +45,3 @@ const ResumeLayout = styled.div`
   gap: 20px; /* 아이템 간 간격 */
   padding: 40px;
 `;
-
